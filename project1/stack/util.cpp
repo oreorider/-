4707e8e64cd1bbec210dd9b5bae2146a7b5badb6
Kyle <yjkim308@gmail.com>
2022-11-03 01:37:10 +0900

    update

--- project1/stack/util.cpp
+++ project1/stack/util.cpp
@@ -8,13 +8,47 @@ using namespace std;
 
 bool checkParentheses(const string& line, const vector<pair<char,char>>& pairs){
     //TODO
+    char popvalue;
     Stack<char> parentheses;
+    for (auto &ch : line){
+        for(auto pair : pairs){
+            if(ch == pair.first){//if the character is first element of pairs
+                parentheses.push(ch);
+                continue;
+            }
+            else if(ch == pair.second){
+                if(parentheses.top() == pair.first){ //if matching, pop
+                    popvalue = parentheses.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    }
     return true;
+}
+
+int precedence(char ch){
+    if(ch == '+' || ch == '-') return 1;
+    if(ch == '*' || ch == '/') return 2;
+    else return 0;
+}
+
+string infix_to_postfix(string line){
+    Stack<char> stk;
+    string returnString = "";
+    for(auto &ch : line){
+        if(int(ch)>=48 && int(ch)<=57){//if number
+
+        }
+    }
 }
 
 float calculate(const string& line){
     //TODO
     Stack<float> numbers;
     Stack<char> operators;
+    string postfix = infix_to_postix(line);
     return 0.f;
 }
