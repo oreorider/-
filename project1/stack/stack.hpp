2bf442907a5d22343573d4f5962f56067ed446ce
Kyle <yjkim308@gmail.com>
2022-11-02 15:24:45 +0900

    push

--- project1/stack/stack.hpp
+++ project1/stack/stack.hpp
@@ -33,31 +33,54 @@ class Stack{
 
 template <typename T>
 bool Stack<T>::isEmpty() const{
-    return true;
+    if(current == -1) return true;
+    else return false;
     
 }
 
 template <typename T>
 bool Stack<T>::isFull() const{
-    //TODO
-    return true;
+    if(current == size-1) return true;
+    else return false;
 }
 
 template <typename T>
 T Stack<T>::top() const{
-    //TODO
-    return NULL;
+    if(isEmpty()){
+        throw underflow_error("stack is empty");
+    }
+    else return array[current];
+    
 }
 
 template <typename T>
 T Stack<T>::pop(){
-    //TODO
-    return NULL;
+    if(isEmpty()){
+        throw underflow_error("stack is emtpy");
+    }
+    auto returnValue = array[current];
+    current-=1;
+    return returnValue;
 }
 
 template <typename T>
 void Stack<T>::push(const T& item){
     //TODO
+    if(isFull()){ //if stack is full, double size, then push
+        auto new_array = new Item<T>[size*2];
+        for(int i=0; i<size;i++){
+            new_array[i] = array[i];//copy elements over
+        }
+        size=size*2;
+        delete[] array;
+        array = new_array;
+        current+=1;
+        array[current] = item;
+    }
+    else{
+        current+=1;
+        array[current] = item;
+    }
     return;
 }
 
