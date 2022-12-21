38cc6dcfce4fd93c0a0e82d57f89781734be8f84
Kyle <yjkim308@gmail.com>
2022-12-22 00:35:28 +0900

    testcase

--- Project2/RedBlackTree/main.cpp
+++ Project2/RedBlackTree/main.cpp
@@ -20,7 +20,7 @@ int main () {
     cout<<"<< RBtree >>"<<endl<<endl;
 
     start_insert_remove = clock();
-    
+    /*
     tree.insert(16545, "yeonnamdng");
     tree.insert(54312, "sinsadong");
     
@@ -35,8 +35,75 @@ int main () {
     tree.insert(51211, "banghwadong");
     
     tree.remove(11243);
+    */
+   /*
+    tree.insert(16, "yf");
+    tree.insert(4, "rg");
+    tree.insert(6, "fj");
+    tree.insert(7, "zm");
+    tree.insert(9, "aq");
+    tree.insert(13, "gw");
+    tree.insert(13, "ry");
+    tree.insert(12, "ve");
+    tree.insert(2, "xo");
+    tree.insert(11, "kz");
+    tree.insert(4, "nv");
+    tree.insert(6, "yx");
+    tree.insert(12, "te");
+    tree.insert(17, "uj");
+    tree.insert(18, "go");
+    tree.insert(9, "wb");
+    tree.insert(10, "an");
+    tree.insert(19, "it");
+    tree.insert(3, "wh");
+    tree.insert(17, "to");
+
+    cout<<"print inorder"<<endl;
+    cout<<"_________________________"<<endl;
+    tree.inorder(tree.root);
+    cout<<"print preorder"<<endl;
+    cout<<"_________________________"<<endl;
+    tree.preorder(tree.root);
+    printBT(tree.root);
     //printBT(tree.root);
-    
+    */
+    tree.insert(34, "ax");
+    tree.insert(6, "jr");
+    tree.insert(7, "zl");
+    tree.insert(12, "fk");
+    tree.insert(1, "fr");
+    tree.insert(13, "ku");
+    tree.insert(20, "ye");
+    tree.insert(39, "qh");
+    tree.insert(43, "hb");
+    tree.insert(17, "kp");
+    tree.insert(29, "ld");
+    tree.insert(2, "sg");
+    tree.insert(18, "ku");
+    tree.insert(31, "pq");
+    tree.insert(44, "pr");
+    tree.insert(8, "mu");
+    tree.insert(5, "br");
+    tree.insert(30, "ij");
+    tree.insert(26, "ud");
+    tree.insert(16, "kp");
+    tree.remove(12);
+    tree.remove(1);
+    tree.remove(18);
+    tree.remove(20);
+    tree.remove(44);
+    tree.remove(2);
+    tree.remove(26);
+    tree.remove(7);
+    tree.remove(8);
+    tree.remove(16);
+    cout<<"print inorder"<<endl;
+    cout<<"_________________________"<<endl;
+    tree.inorder(tree.root);
+    cout<<"print preorder"<<endl;
+    cout<<"_________________________"<<endl;
+    tree.preorder(tree.root);
+    printBT(tree.root);
 
     //add in ascending
     /*
@@ -151,7 +218,7 @@ int main () {
     cout<<endl;
 
     start_search = clock();
-
+    
     cout<<"print search"<<endl;
     cout<<"_________________________"<<endl;
     cout<<"search 54155: "<<tree.search(54155)<<endl;
